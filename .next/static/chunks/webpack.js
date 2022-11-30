a1e4ab298c0854ad5909f73523fa488be68d57aa
BinZ <sikatunar@gmail.com>
2022-11-30 16:42:32 +0800

    Update background

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "da427d1a49776b9e"; }
+/******/ 		__webpack_require__.h = function() { return "5841dc28ef17d1ff"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
